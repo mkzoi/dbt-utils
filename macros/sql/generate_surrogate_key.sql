6aa421936d08dc8e6565f556dadacbc7ff29d0c8
mkzoi <128792860+mkzoi@users.noreply.github.com>
2023-07-13 17:12:05 +0200

    enclose the fieldname by "`"
    Tested w/ BigQuery

--- macros/sql/generate_surrogate_key.sql
+++ macros/sql/generate_surrogate_key.sql
@@ -15,7 +15,7 @@
 {%- for field in field_list -%}
 
     {%- do fields.append(
-        "coalesce(cast(" ~ field ~ " as " ~ dbt.type_string() ~ "), '" ~ default_null_value  ~"')"
+        "coalesce(cast(`" ~ field ~ "` as " ~ dbt.type_string() ~ "), '" ~ default_null_value  ~"')"
     ) -%}
 
     {%- if not loop.last %}
